3750d91e1c460317d0f19784c46b6e69765a87b3
soyeonim <soyeonim@zuminternet.com>
2023-05-22 23:59:22 +0900

    hdfs, aws 사용 툴

---
+++ tool/aws_command.py
@@ -0,0 +1,85 @@
+import boto3
+
+class AwsCommand:
+
+  def __init__(self, profile:str):
+    self._session = boto3.Session(profile_name=profile, region_name='ap-northeast-2')
+    self._s3_client = None
+    self._s3 = None
+    self._command = []
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, exec_type, exec_value, traceback):
+    if self._s3:
+      del self._s3
+    if self._session:
+      del self._session
+  
+  def close(self):
+    self.__exit__()
+
+  @property
+  def s3(self):
+    if not self._s3_client:
+      self._s3_client = self._session.client('s3')
+    if not self._s3:
+      self._s3 = self._session.resource('s3')
+    return self
+
+  def bucket(self, bucket_name:str, prefix:str):
+    self._bucket_name = bucket_name
+    self._prefix = prefix
+    self._bucket = self._s3.Bucket(bucket_name)
+    return self
+
+  def show_buckets(self):
+    for bucket in self._s3.buckets.all():
+      print(f'- {bucket.name}')
+
+  def show_objects(self, prefix:str, is_files=False):
+    prefix = prefix if prefix else self._prefix
+    objects = self._bucket.objects.filter(Prefix=prefix)
+    print(len(list(objects)))
+    for obj in objects:
+      print(obj)
+      if is_files is False and obj.key.endswith('/') is False:
+        continue
+      print(f'- {obj.key}')
+
+  def is_empty(self, prefix:str):
+    objects = list(self._bucket.objects.filter(Prefix=prefix))
+    return True if len(objects) == 0 else False
+  
+  def get_info(self, prefix:str):
+    res = self._s3_client.list_objects_v2(Bucket=self._bucket_name, Prefix=prefix)
+    contents = res.get('Contents')
+    total_size = 0
+    for data in contents:
+      total_size += data.get('Size', 0)
+    return { 'count' : len(contents), 'total_size' : total_size }
+
+  def upload_files(self, files:str, obj_name:str):
+    return self._s3_client.upload_file(files, self._bucket_name, obj_name) if obj_name else False
+
+  def delete_files(self, prefix:str):
+    res = self._s3_client.list_objects_v2(Bucket=self._bucket_name, Prefix=prefix)
+    for obj in res.get('Contents'):
+      print(f'Deleing {obj["Key"]}')
+      self._s3_client.delete_object(Bucket=self._bucket_name, Key=obj['Key'])
+
+
+if __name__ == "__main__":
+
+  # bigdata_s3
+  with AwsCommand('default') as aws:
+    #aws.s3.show_buckets()
+    aws.s3.bucket('my-project','data')
+    #aws.show_objects()
+    #if aws.upload_files('/data/test.txt', 'data/test.txt') is False:
+    #  print('Failed uploading files!!')
+
+    print('is_empty=', aws.is_empty(prefix='data'))
+    #print(aws.get_info('data/20230501'))
+    #aws.delete_files('data/20230501')
---
+++ tool/hdfs_command.py
@@ -0,0 +1,94 @@
+import subprocess
+
+class HdfsResult:
+
+  def __init__(self, is_success:bool, stdout:str, stderr:str):
+    self._is_success = is_success
+    self._stdout = stdout
+    self._stderr = stderr
+
+  def __str__(self):
+    if self.isSuccess is True:
+      return f'{self._stdout}'
+    else:
+      return f'{self._stderr}'
+
+  @property
+  def isSuccess(self):
+    return self._is_success
+
+  @property
+  def result(self):
+    return self._stdout
+
+class HdfsCommand:
+
+  def __init__(self):
+    self._command = []
+    self._options = []
+    self._args = []
+
+  def ls(self, path:str):
+    self._command.extend(['hdfs', 'dfs', '-ls'])
+    self._args.extend([path])
+    return self
+
+  def count(self, path:str):
+    self._command.extend(['hdfs', 'dfs', '-count'])
+    self._args.extend([path])
+    return self
+
+  def get(self, source:str, target:str):
+    self._command.extend(['hdfs', 'dfs', '-get'])
+    self._args.extend([source, target])
+    return self
+ 
+  def distcp(self, source:str, target:str):
+    self._command.extend(['hadoop', 'distcp'])
+    self._args.extend([source, target])
+    return self
+
+  def options(self, options:list):
+    self._options.extend(options)
+    return self
+
+  def run(self):
+    '''
+    '''
+    hdfs_command = []
+    try:
+      if not self._command:
+        raise Exception('Invalid hdfs cli')
+
+      hdfs_command = self._command + self._options + self._args
+      result = subprocess.run(hdfs_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      if result.returncode != 0:
+        raise Exception(f'Failed Command {result.stderr.decode()}')
+    except Exception as e:
+      return HdfsResult(False, result.stdout.decode(), f'{hdfs_command}, Error Msg:{e}')
+    finally:
+      self.__init__()
+    return HdfsResult(True, result.stdout.decode(), result.stderr.decode())
+
+if __name__ == "__main__":
+
+  import os
+  hdfs = HdfsCommand()
+
+  # ls usage
+  #print(hdfs.ls('/data/20230501').run())
+
+  # distcp
+#  print(hdfs.distcp(source='/data/20230501', target='/data_test/20230501').options(['-skipcrccheck']).run())
+
+  # get
+#  os.makedirs('local/path/data', exist_ok=True) 
+#  print(hdfs.get(source='/data/20230501', target=target_fs_path,).run())
+
+  # count
+  #print(hdfs.count(path='/data/20230501').options(['-h']).run())
+
+  ret = hdfs.count(path='/data/20230501').run()
+  print(f'{ret.result}')
+
+
